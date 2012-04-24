a240b0c8ce8926c1a1591e15ba5c02fe552e1af8
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-04-24 22:31:12 +0200

    AssociativyTagNode is re-set into AssociativyGraphPart as contained type if removed

--- Handlers/AssociativyTagGraphPartHandler.cs
+++ Handlers/AssociativyTagGraphPartHandler.cs
@@ -5,6 +5,8 @@ using System.Text;
 using Orchard.ContentManagement.Handlers;
 using Orchard.Data;
 using Associativy.TagsAdapter.Models;
+using Associativy.Administration.Models;
+using Orchard.ContentManagement;
 
 namespace Associativy.TagsAdapter.Handlers
 {
@@ -13,6 +15,18 @@ namespace Associativy.TagsAdapter.Handlers
         public AssociativyTagGraphPartHandler(IRepository<AssociativyTagGraphPartRecord> repository)
         {
             Filters.Add(StorageFilter.For(repository));
+
+            OnUpdated<AssociativyGraphPart>((context, part) =>
+                {
+                    if (!part.As<AssociativyTagGraphPart>().IsTagGraph) return;
+
+                    if (!part.ContainedContentTypes.Contains("AssociativyTagNode"))
+                    {
+                        part.ContainedContentTypes.Add("AssociativyTagNode");
+                        // This is required as serialization only runs when the setter runs
+                        part.ContainedContentTypes = part.ContainedContentTypes;
+                    }
+                });
         }
     }
 }
