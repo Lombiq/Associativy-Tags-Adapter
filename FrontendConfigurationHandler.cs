b0ee10d537febb411d28bbc8b358bd0dd96d23c2
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-04-25 01:32:35 +0200

    Removing unnecessary configuration

--- FrontendConfigurationHandler.cs
+++ FrontendConfigurationHandler.cs
@@ -15,32 +15,14 @@ using Orchard.Core.Title.Models;
 
 namespace Associativy.TagsAdapter
 {
-    // Here other configuration handler can be implemented too
-    public class FrontendConfigurationHandler : IFrontendEngineEventHandler, IJITConfigurationHandler
+    // Here other configuration handlers can be implemented too
+    public class FrontendConfigurationHandler : IJITConfigurationHandler
     {
         private readonly IGraphManager _graphManager;
 
         public FrontendConfigurationHandler(IGraphManager graphManager)
         {
             _graphManager = graphManager;
-        }
-
-        public void OnPageInitializing(IEngineContext engineContext, IGraphContext graphContext, IContent page)
-        {
-        }
-
-        public void OnPageInitialized(IEngineContext engineContext, IGraphContext graphContext, IContent page)
-        {
-            if (!_graphManager.FindGraph(graphContext).ContentTypes.Contains("AssociativyTagNode")) return;
-
-            // This will cause bad performance with repeated queries, should be addressed.
-            page.As<IEngineConfigurationAspect>().MindSettings.ModifyQuery = (query) =>
-                {
-                };
-        }
-
-        public void OnPageBuilt(IEngineContext engineContext, IGraphContext graphContext, IContent page)
-        {
         }
 
         public void SetupViewModel(IEngineContext engineContext, IGraphContext graphContext, IContent node, Frontends.Engines.JIT.ViewModels.NodeViewModel viewModel)
