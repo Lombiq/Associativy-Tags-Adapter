11d140331acf6d9035cc36846695cbb2870b3498
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-05-06 23:48:55 +0200

    Adpating to JIT rename

--- FrontendConfigurationHandler.cs
+++ FrontendConfigurationHandler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Associativy.Frontends.Engines.JIT;
+using Associativy.Frontends.Engines.Jit;
 using Associativy.Frontends.Engines;
 using Associativy.GraphDiscovery;
 using Orchard.ContentManagement;
@@ -16,7 +16,7 @@ using Orchard.Core.Title.Models;
 namespace Associativy.TagsAdapter
 {
     // Here other configuration handlers can be implemented too
-    public class FrontendConfigurationHandler : IJITConfigurationHandler
+    public class FrontendConfigurationHandler : IJitConfigurationHandler
     {
         private readonly IGraphManager _graphManager;
 
@@ -25,7 +25,7 @@ namespace Associativy.TagsAdapter
             _graphManager = graphManager;
         }
 
-        public void SetupViewModel(FrontendContext frontendContext, IContent node, Frontends.Engines.JIT.ViewModels.NodeViewModel viewModel)
+        public void SetupViewModel(FrontendContext frontendContext, IContent node, Frontends.Engines.Jit.ViewModels.NodeViewModel viewModel)
         {
             if (node.ContentItem.ContentType == "AssociativyTagNode")
             {
